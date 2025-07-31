52ef7d27a57e04d654bc1255c300bca101e1570f
Brayden Todd Sundberg <sundberg3@ruby968.llnl.gov>
2025-07-31 12:47:19 -0700

    Updated logic for Molanal species warning

--- src/verify_config.py
+++ src/verify_config.py
@@ -758,23 +758,6 @@ def verify(user_config):
         exit()    
     else:    
         user_config.NO_CASES = int(user_config.NO_CASES)
-        
-
-    if not hasattr(user_config,'MOLANAL_SPECIES'):
-
-        # Species to track/plot from molanal... only used for post-processing
-        # MOLANAL_SPECIES
-        #MOLANAL_SPECIES = ["C1 O1 1(O-C)",
-            #               "C1 O2 2(O-C)",
-            #               "N2 1(N-N)",
-            #               "N1 O1 1(O-N)",
-            #               "N2 O1 1(N-N) 1(O-N)",
-            #               "O2 1(O-O)"]#
-
-        print("WARNING: Option config.MOLANAL_SPECIES was not set")
-        print("         Will set to no species:")
-        
-        user_config.MOLANAL_SPECIES = [""]
 
     if not hasattr(user_config,'USE_AL_STRS'):
 
@@ -1344,6 +1327,21 @@ def verify(user_config):
         print("WARNING: MOLANAL will not be ran.")
         print("         RUN_MOLANAL set to False")
     
+    if not hasattr(user_config,'MOLANAL_SPECIES') and user_config.RUN_MOLANAL:
+
+        # Species to track/plot from molanal... only used for post-processing
+        # MOLANAL_SPECIES
+        #MOLANAL_SPECIES = ["C1 O1 1(O-C)",
+            #               "C1 O2 2(O-C)",
+            #               "N2 1(N-N)",
+            #               "N1 O1 1(O-N)",
+            #               "N2 O1 1(N-N) 1(O-N)",
+            #               "O2 1(O-O)"]#
+
+        print("WARNING: Option config.MOLANAL_SPECIES was not set")
+        print("         Will set to no species.")
+        
+        user_config.MOLANAL_SPECIES = [""]
     
         
     if not hasattr(user_config,'MDFILES'):
